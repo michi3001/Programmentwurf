c5b48cf21189a69f269f8601d7357e51ee132376
michi3001 <124174958+michi3001@users.noreply.github.com>
2023-04-12 11:55:03 +0200

    Beautified main.c

--- CLion_Projekt/TINF20_Prog1_5368572/main.c
+++ CLion_Projekt/TINF20_Prog1_5368572/main.c
@@ -17,13 +17,13 @@ int main() {
     printf("\nYOU CAN CHANGE THE AMOUNT OF EQUIDISTANT LINES WITH THE CONSTANT \"PARAMETER\" WHICH YOU CAN FIND IN THE HEADER FILE \"constants.h\"");
 
     fillMatrix(cMatrix);
-    drawCircle(cMatrix, '-', 200);              //Draw the inner circle
-    calc_equidistant_points(&sInnerPoints, 200);     //Fill the inner circle with equidistant points
-    drawCircle(cMatrix, '*', 400);              //Draw the outer circle
-    calc_equidistant_points(&sOuterPoints, 400);     //Fill the inner circle with equidistant points
+    
+    drawCircle(cMatrix, '-', 200);                  //Draw the inner circle
+    calc_equidistant_points(&sInnerPoints, 200);    //Fill the inner circle with equidistant points
+    drawCircle(cMatrix, '*', 400);                  //Draw the outer circle
+    calc_equidistant_points(&sOuterPoints, 400);    //Fill the inner circle with equidistant points
 
     connect_points(cMatrix, &sInnerPoints, &sOuterPoints);
-
 
     create_ppm(cMatrix);
 
