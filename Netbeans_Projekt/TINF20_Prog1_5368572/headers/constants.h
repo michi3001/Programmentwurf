0acadf1fc0f7121c89c807fce48407736e22c3a7
Michael Schick <michael.schick@gmx.net>
2023-04-14 10:07:59 +0200

    beautified constants.h

--- Netbeans_Projekt/TINF20_Prog1_5368572/headers/constants.h
+++ Netbeans_Projekt/TINF20_Prog1_5368572/headers/constants.h
@@ -6,10 +6,10 @@
 #define TINF20_PROG1_5368572_CONSTANTS_H
 
 // CONSTANTS
-#define FILEPATH "PPM_Files/Filename.ppm"     // !!!Defines the name and the location of the generated file!!!
-                                                    // If you want to add the file into an folder you have to create the folder at first
-                                                    // In this case you have to create the folder PPM_Files at the same location where the 
-                                                    // main.c file is stored
+#define FILEPATH "PPM_Files/Filename.ppm"       // !!!Defines the name and the location of the generated file!!!
+                                                // If you want to add the file into an folder you have to create the folder at first
+                                                // In this case you have to create the folder PPM_Files at the same location where the 
+                                                // main.c file is stored
 
 #define X_PIXEL 1001 // ppm file width
 #define Y_PIXEL 1001 // ppm file height
